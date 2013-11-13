b7be3e82f3c42749a33d25bb8ef88986b1fe63db
McAwesomesauce <crispy.carpenter@gmail.com>
2013-11-13 21:01:44 +0800

    fixed the Wooden Crossbow class so it compiles

--- crossBow.java
+++ crossBow.java
@@ -2,6 +2,7 @@ public class crossBow extends weapon
 {
     public crossBow(int ammo)
     {
-        super.weaponClass("Wooden Crossbow", "melee", 10.0, 1, 0.5, ammo, true, 1);
+        // Wooden Crossbow, 3kg, uses ammo decided by constructor, 5 damage, 0.00 critical bonus)
+        super("Wooden Crossbow", 3, ammo, 5, 0);
     }
 }
